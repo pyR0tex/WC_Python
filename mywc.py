e1b1a15e8f567ad5343c92bb29a22966e7735b0c
Ro <rohit6cheruku@gmail.com>
2024-06-17 15:18:17 -0700

    First Commit. Used venv and generated a requirements.txt. This is all so new to me

---
+++ mywc.py
@@ -0,0 +1,15 @@
+# mywc.py
+
+'''
+Python clone of the UNIX program: * wc *
+
+
+'''
+
+
+def main():
+    """Python clone of the UNIX program: * wc *"""
+    pass
+
+if __name__ == '__main__':
+    main()
