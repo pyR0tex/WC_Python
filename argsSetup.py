541eee7f2a5bd30bbd10c64fa04559b09e122453
Rohit Cheruku <rohit6cheruku@gmail.com>
2024-06-19 01:16:10 -0700

    added -c functionality. Need to learn and understand more

--- argsSetup.py
+++ argsSetup.py
@@ -9,10 +9,26 @@ import argparse
 import sys
 
 def argsSetup():
-    
+    '''
+    Initialize an instance of argparse
+    parser: container for argument specification and has 
+            options that apply to parser as a whole
+    '''
     parser = argparse.ArgumentParser(
+        prog="mywc",
         description="Python clone of the UNIX program: * wc *"
     )
-    parser.add_argument('echo', type=str, nargs='*', help='echoing')
+   
+    '''
+    Flag: -c
+    Def: Number of bytes in each input file is written to the standard output
+    '''
+    parser.add_argument('-c',
+                        nargs='?',
+                        type=argparse.FileType("r"),
+                        const=sys.stdin,
+                        help="The number of bytes in each input file is written to standard output"
+                        )
+    
     args = parser.parse_args()
     return args
--- mywc.py
+++ mywc.py
@@ -1,5 +1,5 @@
 # mywc.py
-'''
+''' 
 Python clone of the UNIX program: * wc *
 
 Author: Rohit Cheruku
@@ -7,15 +7,50 @@ Author: Rohit Cheruku
 
 # Imports
 from argsSetup import argsSetup
+from os import path, remove
+import sys
 
 def main(args):
     """ Python clone of the UNIX program: * wc * """
-    print(" Python clone of the UNIX program: * wc * ")
-    if args.echo:
-        print("hello")
-    pass
+    #print(" Python clone of the UNIX program: * wc * ")
+    
+    # Start of if cases for input file / flags
+    # if -c flag
+    if args.c:
+        if args.c.name != "<stdin>":
+            print(f"        {Byte_Count(args.c.name)} {path.basename(args.c.name)}")
+        else:
+            print(f"        {Byte_Count(args.c.name)}")
+
+def Byte_Count(fileName):
+    ''' Returns the number of bytes in each Input File
+
+    Parameters:
+        fileName (str): standard input (stdin) or Path to a file
+
+    Return:
+        str: number of bytes in file
+    
+    Exceptions:
+        FileNotFoundError: If file not found or name is invalie
+        OSError: If any other exception occurs
+    '''
+    try:
+        if fileName == "<stdin>":
+            fileContent = sys.stdin.read()
+            numBytes = len(bytes(fileContent, "utf-8"))
+            return str(numBytes)
+        else:
+            try:
+                numBytes = path.getsize(fileName)
+                return numBytes
+            except FileNotFoundError:
+                return f"File: {path.basename(fileName)} not found"
+    except OSError:
+        return "OS Error occured"
+
 
 if __name__ == '__main__':
+    # Get list of arguments for argparse
     args = argsSetup()
-
     main(args)
